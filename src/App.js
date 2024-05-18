ce13d60f356b795f56ca56e6b7c44a8ced64e4a8
Ayush Tiwari <ayush@Ayushs-MacBook-Pro.local>
2024-05-18 15:26:35 +0530

    Demo app ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, {useState} from 'react';
+import bear from './bear-panda.gif'
 
-function App() {
+
+function Boxes() {
+  const [inputText, setInputText] = useState('');
+  const [imageUrl, setImageUrl] = useState(bear);
+  const [serverCall, setServerCall] = useState(false)
+  const serverUrl = "http://10.8.18.122:80"
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleReset = () =>{
+    setImageUrl(bear);
+  }
+
+  const handleSubmit = async () => {
+    setServerCall(true)
+    const response_generate = await axios.post(`${serverUrl}/generate`,{
+      prompt: inputText
+    })
+
+    // console.log("Data:", response_generate.data.image_path)
+    const response = await axios.get(`${serverUrl}/get_image`,{
+      params:{
+        image_path:response_generate.data.image_path
+      },
+      responseType:'blob'
+    })
+
+    const blob = new Blob([response.data], { type: 'image/png' });
+    const url = URL.createObjectURL(blob);
+    setImageUrl(url);
+    setServerCall(false)
+    
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-fluid'>
+      <div className='row justify-content-center align-items-center' style={{ height: '100vh' }}>
+        <div className='col-md-6'>
+          <div className='row align-items-center'>
+            <div className='col'>
+              <input className='form-control mb-3' placeholder='Enter image description' value={inputText} onChange={handleInputChange} style={{ width: '300px', height: '100px' }}></input>
+              <div className='text-center'>
+                <div style={{ marginBottom: '10px' }}>
+                    <button className="btn btn-primary" onClick={handleSubmit} disabled={serverCall}>{serverCall ? 'Generating Image...' : 'Generate Image'}</button>
+                </div>
+                <div>
+                    <button className="btn btn-primary" onClick={handleReset} disabled={serverCall}>Reset</button>
+                </div>
+              </div>
+            </div>
+            <div className='col' style={{ width: '400px', height: '400px', border: '1px solid black' }}>
+              {imageUrl && <img src={imageUrl} style={{
+                width: '100%', 
+                height: '100%',
+                padding: '5px'
+            }}  alt="Image" />}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Boxes;
---
+++ src/Title.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function FaceAlexa() {
+  return <h1 style={{ margin: '20px' }}>FaceAlexa</h1>;
+}
+
+export default FaceAlexa;
